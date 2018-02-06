87ba32f0fb1c73e6d5e0704c5584a267c5013dd9
dmitryl <dmitryl@imaginet.com>
2018-02-06 08:59:37 -0600

    Commit 1

---
+++ SpyStore.DAL/EF/MyConnectionStrategy.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace SpyStore.DAL.EF
+{
+    public class MyExecutionStrategy : ExecutionStrategy
+    {
+        public MyExecutionStrategy(DbContext context) :
+            this(context, DefaultMaxRetryCount, DefaultMaxDelay)
+        {
+        }
+
+        public MyExecutionStrategy(DbContext context, int maxRetryCount, TimeSpan maxRetryDelay) :
+            base(context, maxRetryCount, maxRetryDelay)
+        {
+        }
+
+        public MyExecutionStrategy(ExecutionStrategyDependencies dependencies) :
+            this(dependencies, DefaultMaxRetryCount, DefaultMaxDelay)
+        {
+        }
+
+        public MyExecutionStrategy(ExecutionStrategyDependencies dependencies, int maxRetryCount, TimeSpan maxRetryDelay) :
+            base(dependencies, maxRetryCount, maxRetryDelay)
+        {
+        }
+
+        protected override bool ShouldRetryOn(Exception exception)
+        {
+            return true;
+        }
+    }
+}
---
+++ SpyStore.DAL/EF/StoreContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpyStore.DAL.EF
+{
+    public class StoreContext: DbContext
+    {
+        public StoreContext()
+        {
+
+        }
+        public StoreContext(DbContextOptions options) :base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source = DESKTOP - V0ME6CC; Initial Catalog = SpyStore; Integrated Security = True",
+                    //options=>options.EnableRetryOnFailure()); //-default connection strategy
+                    options => options.ExecutionStrategy(c => new MyExecutionStrategy(c))); // - custom connection strategy
+                    //Data Source = DESKTOP - V0ME6CC; Initial Catalog = SpyStore; Integrated Security = True
+
+            }
+        }
+    }
+}
